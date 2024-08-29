2d74e92b60340beca70001bbcb104cfc00e1562a
ray <raychongtk@gmail.com>
2024-08-29 14:03:29 +0800

    support get logger by class

--- src/main/java/com/canonicallog/logging/core/CanonicalLoggerFactory.java
+++ src/main/java/com/canonicallog/logging/core/CanonicalLoggerFactory.java
@@ -8,6 +8,10 @@ import java.util.concurrent.ConcurrentHashMap;
 public class CanonicalLoggerFactory {
     private static final Map<String, Logger> loggers = new ConcurrentHashMap<>();
 
+    public static Logger getLogger(Class<?> clazz) {
+        return getLogger(clazz.getName());
+    }
+
     public static Logger getLogger(String name) {
         return loggers.computeIfAbsent(name, (k) -> new CanonicalLogger(name));
     }
