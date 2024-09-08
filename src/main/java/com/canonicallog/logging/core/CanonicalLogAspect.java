968ae632ee6d50a66a554cecd7bb7de89c79313f
ray <raychongtk@gmail.com>
2024-09-08 12:46:17 +0800

    attempt put http metadata to canonical log

--- src/main/java/com/canonicallog/logging/core/CanonicalLogAspect.java
+++ src/main/java/com/canonicallog/logging/core/CanonicalLogAspect.java
@@ -1,11 +1,15 @@
 package com.canonicallog.logging.core;
 
 import com.canonicallog.logging.annotation.CanonicalLog;
+import jakarta.servlet.http.HttpServletRequest;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Aspect
 @Component
@@ -20,6 +24,8 @@ public class CanonicalLogAspect {
             logTrace.put("class_name", joinPoint.getSignature().getDeclaringType().getName());
             logTrace.put("method_name", joinPoint.getSignature().getName());
 
+            attemptPutHttpMetadata(logTrace);
+
             long startTime = System.currentTimeMillis();
             Object result = joinPoint.proceed();
             long endTime = System.currentTimeMillis();
@@ -28,6 +34,23 @@ public class CanonicalLogAspect {
             return result;
         } finally {
             canonicalLogTracer.finish();
+        }
+    }
+
+    private void attemptPutHttpMetadata(CanonicalLogTrace logTrace) {
+        try {
+            RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+            ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) requestAttributes;
+            if (servletRequestAttributes != null) {
+                HttpServletRequest request = servletRequestAttributes.getRequest();
+                logTrace.put("http_method", request.getMethod());
+                logTrace.put("uri", request.getRequestURI());
+                String queryString = request.getQueryString();
+                if (queryString != null) {
+                    logTrace.put("query_string", queryString);
+                }
+            }
+        } catch (Exception ignore) {
         }
     }
 }
